a9f1ad0ad7f5b0b1bb9c204ef8aae51c2ad26fcd
zy410419243 <410419243@qq.com>
2018-04-02 23:15:31 +0800

    update
    1、增加柱子渲染及高度过渡

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
     // 初始化参数
     this._startPositions = {};
     this._startTweenCount = 0;
+    // 柱子高度变化的定时器
+    this._intervals = {};
 
     // 初始化three渲染三要素
     var renderer = new THREE.WebGLRenderer();
@@ -54,73 +56,99 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
 
         objLoader.setMaterials(materials);
         objLoader.load('./data/model/deqing.obj', function (object) {
-            object.traverse(function (child) {
-                if (child instanceof THREE.Group) {
-                    return;
-                }
-                if (child instanceof THREE.Mesh) {
-                    child.geometry = new THREE.Geometry().fromBufferGeometry(child.geometry);
-                } else {
-                    if (child instanceof THREE.Line) {
-                        child.material.lights = false;
+            // 请求业务数据
+            fetch('./data/simulation.json').then(function(response) {
+                return response.json();
+            }).then(function(result) {
+                object.traverse(function (child) {
+                    if (child instanceof THREE.Group) {
+                        return;
                     }
-                }
-                // 初始化动画参数
-                _dealObjectInLoadCirculStart(child);
-                if (child.material) {
-                    child.initialMaterial = child.material.clone();
-                }
-
-                // 开始动画
-                var name = child.name.split('_');
-				var last_name = name[name.length - 1];
-                var area = name[0];
-                
-                if (area !== "china") {
-					if(!this._startPositions[area]) {
-						return;
-					}
-
-					// 定义各板块移动速度
-					var time = this._startPositions[area].time;
-					if (!time) {
-						time = Math.random() * this._duration * 5 + this._duration;
-						this._startPositions[area].time = time;
-					}
-					time = 2000;
-
+                    if (child instanceof THREE.Mesh) {
+                        child.geometry = new THREE.Geometry().fromBufferGeometry(child.geometry);
+                    } else {
+                        if (child instanceof THREE.Line) {
+                            child.material.lights = false;
+                        }
+                    }
+                    // 初始化动画参数
+                    _dealObjectInLoadCirculStart(child);
+                    if (child.material) {
+                        child.initialMaterial = child.material.clone();
+                    }
+    
                     // 开始动画
-					var tween = new TWEEN.Tween(child.position)
-						.to({x: 0, y: 0, z: 0}, time)
-						.easing(TWEEN.Easing.Exponential.InOut)
-						.onComplete(function () {
-							this._startTweenCount--;
-							if (this._startTweenCount === 0) {
-                                console.log('complete')
-								// this._afterMovement();
-							}
-                        }.bind(this));
-                    tween.start();
-					this._startTweenCount++;
-				}
+                    var name = child.name.split('_');
+                    var last_name = name[name.length - 1];
+                    var area = name[0];
+                    
+                    if (area !== "china") {
+                        if(!this._startPositions[area]) {
+                            return;
+                        }
+    
+                        // 定义各板块移动速度
+                        var time = this._startPositions[area].time;
+                        var duration = 1000;
+                        if (!time) {
+                            time = Math.random() * duration * 5 + duration;
+                            this._startPositions[area].time = time;
+                        }
+                        time = 2000;
+    
+                        // 开始动画
+                        var tween = new TWEEN.Tween(child.position)
+                            .to({x: 0, y: 0, z: 0}, time)
+                            .easing(TWEEN.Easing.Exponential.InOut)
+                            .onComplete(function () {
+                                this._startTweenCount--;
+                                if (this._startTweenCount === 0) {
+                                    // 求最大值，用于计算柱子高度
+                                    var maxData = 0;
+                                    // 处理业务数据和模型数据，使之一一对应
+                                    for(var i = 0; i < result.length; i++){
+                                        var item = result[i];
+
+                                        maxData = Math.max(item.val, maxData);
+
+                                        for(var j = 0; j < object.children.length; j++){
+                                            var jtem = object.children[j];
+
+                                            if(item.stcd == jtem.name.split('_')[0] && /_pillar$/.test(jtem.name)){
+                                                jtem.userData = item;
+                                            }
+                                        }
+                                    }
+
+                                    // 渲染柱子
+                                    object.traverse(function (child) {
+                                        _changeModel4DataRefresh(child, object, maxData);
+                                    });
+                                    _afterMovementMesh(root, scene, camera, renderer);
+                                }
+                            }.bind(this));
+                        tween.start();
+                        this._startTweenCount++;
+                    }
+                });
+    
+                /* 
+                    渲染模型容器
+                    相当于对div进行appendChild，
+                    原因是scene上不能直接渲染Mesh啊Group之类的对象
+                */
+                var root = new THREE.Object3D();
+                root.add(object);
+                
+                scene.add(root);
+    
+                function render(){
+                    requestAnimationFrame(render);
+                    TWEEN.update();
+                    renderer.render(scene, camera);
+                }
+                render();
             });
-
-            /* 
-                渲染模型容器
-                相当于对div进行appendChild，
-                原因是scene上不能直接渲染Mesh啊Group之类的对象
-            */
-            var root = new THREE.Object3D();
-            root.add(object);
-            
-            scene.add(root);
-
-            function render(){
-                requestAnimationFrame(render);
-                TWEEN.update();
-                renderer.render(scene, camera);
-            }
-            render();
         });
     });
 
@@ -139,13 +167,12 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
 
     function _dealObjectInLoadCirculStart(child) {
         if(!child.name) {
-			return ;
+			return;
 		}
 		child.receiveShadow = this._shadow;
 		if (/_pillar$/.test(child.name)) {
 			child.castShadow = this._shadow;
 		} else if (/^(\w*_border)|(china_\w*)$/.test(child.name)) {
-			// 中国及边线不显示，南海诸岛边线默认显示
 			if ('nhzd_border' !== child.name) {
 				child.visible = false;
 			}
@@ -164,4 +191,130 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
             }
         }
     }
+
+    /* 
+		完成开场动画后，相机视角变动
+		一种是旋转mesh，一种是改变相机位置
+		这是旋转mesh
+	*/
+	function _afterMovementMesh(root, scene, camera, renderer) {
+		var rotateAnimate = function(){
+			requestAnimationFrame(rotateAnimate);
+			
+			// y轴正半轴朝上
+			// this.root.position.y <= 0 ? this.root.position.y += 0.8 : null;
+			// 沿x轴旋转
+			root.rotation.x >= -Math.PI / 4 ? root.rotation.x -= 0.03 : null;
+			renderer.render(scene, camera);
+		};
+
+		rotateAnimate();
+    }
+    
+    function _changeModel4DataRefresh(child, object, maxData) {
+        var name = child.name;
+        
+		if (/_pillar$/.test(name)) {
+            var dmName = name.split('_')[0];
+            var data = child.userData.val;
+			if(dmName) {
+				var height = 0;
+				var proportion = 0;
+
+				if (maxData && 0 !== maxData) {
+					proportion = data / maxData;
+					height = proportion * 15;
+				}
+
+				// 不支持负数，但万一传了负数，暂按0处理
+				if(height <= 0) {
+					child.visible = false;
+				} else {
+					_setHeightSlow(child, height);
+					child.visible = true;
+					// var o = object.getObjectByName(dmName);
+					// o._data = data;
+				}
+			} else {
+				dmName = name ? name.split("_")[0] : '';
+				// TODO 与上面height<=0重复，代码需精简
+				child.visible = false;
+			}
+		}
+    };
+    
+    /**
+	 * 设置柱子高度（缓慢变高，高度从1开始变到指定高度）
+	 * 这个方法不要加太多逻辑，这里我试着在循环里console.log，渣电脑甚至能掉帧
+	 * @param child
+	 * @param height
+	 * @private
+	 */
+	function _setHeightSlow(child, height) {
+		var self = this;
+		_setHeight(child, 1);
+		var i = 1;
+        var sh;
+        // 柱子高度上升速度
+		var times = 300;
+
+		function show() {
+			if (i < times) {
+                var h = Math.floor(height * i / times);
+				_setHeight(child, h);
+				i++;
+			} else if (i === times) {
+				i++;
+				_setHeight(child, height);
+			} else {
+				clearInterval(sh);
+				delete self._intervals[sh];
+			}
+		}
+
+		sh = setInterval(show, self._hightslowinterval);
+		this._intervals[sh] = sh;
+    };
+    
+    /**
+	 * 设置模型高度
+	 * 这个方法不要加太多逻辑，这里我试着在循环里console.log，渣电脑甚至能掉帧
+	 * @param object
+	 * @param height
+	 * @private
+	 */
+	function _setHeight(object, height) {
+		if (height === 0 || isNaN(height)) {
+			// 不能高度设置为0，否则下一次设置的时候会出问题，
+			height = 1;
+		}
+		var geometry = object.geometry;
+
+		geometry.verticesNeedUpdate = true;
+		var vertices = geometry.vertices;
+
+		/* 
+			此处应该当有掌声，当然可能写的人的特有感慨
+			这里区分了顶面和底面的点，前提是给的模型数据里的柱子高度不能为0
+
+			下面这段代码看上去简单，但领悟到顶面和底面不能在同一个面上,否则就区分不出的痛整整花了一天时间
+
+			这里先区分z，不是特别花俏的柱子一般只有六个面八个点两种z
+			然后底面的z不变，顶面的z加高度
+		*/
+		var minz = vertices[0].z;
+		for (var i = 1, size = vertices.length; i < size; i++) {
+			var z = vertices[i].z;
+			if (minz != z) {
+				minz = Math.min(z, minz);
+				break;
+			}
+		}
+		for (var i = 0, size = vertices.length; i < size; i++) {
+			var vertice = vertices[i];
+			if (vertice.z !== minz) {
+				vertice.z = minz + height;
+			}
+		}
+	};
 });
