0e2d7cf6d3da1f04ce535516bdd6a1f012d99511
zy410419243 <410419243@qq.com>
2018-04-04 15:18:54 +0800

    update
    1、增加点击详情
    2、但数据没传过来，Raycaster.intersectObjects居然返回了新对象，也就是说我之前绑在userData里的数据全没了

---
+++ config.js
@@ -0,0 +1,9 @@
+define([], function (){
+    return {
+        line_color: '#045AAF', // 内部乡镇边界贴图
+        pillar_color: '#1E8FF7', // 柱子贴图
+        bottom_color: '#0E2C6A', // 底部贴图
+        top_color: '#071C5B', // 上表面贴图
+        select_color: '#F96', // 鼠标移入时贴图
+    };
+})
--- main.js
+++ main.js
@@ -4,11 +4,12 @@ require.config({
         'mtl-loader': './third/three/loaders/MTLLoader',
         'obj-loader': './third/three/loaders/OBJLoader',
         'orbitControls': './third/three/controls/OrbitControls',
-        'tween': './third/tween/Tween'
+        'tween': './third/tween/Tween',
+        'config': './config'
     },
 })
 
-require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], function(THREE, MTLLoader, OBJLoader, OrbitControls, TWEEN){
+require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween', 'config'], function(THREE, MTLLoader, OBJLoader, OrbitControls, TWEEN, _config){
     var clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
     var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
     var container = document.getElementById('container');
@@ -18,6 +19,11 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
     this._startTweenCount = 0;
     // 柱子高度变化的定时器
     this._intervals = {};
+    // 鼠标移入板块高亮
+    this._old = null;
+    this._current = null;
+    // 配置
+    this._config = _config;
 
     // 初始化three渲染三要素
     var renderer = new THREE.WebGLRenderer();
@@ -32,9 +38,9 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
     scene.add(camera);
 
     // 创建环境光，参数为十六进制的颜色
-    var ambientLight = new THREE.AmbientLight(0xaaaaaa)
+    var ambientLight = new THREE.AmbientLight(0xaaaaaa);
     // 创建定向光源，参数为十六进制的颜色
-    var directionalLight = new THREE.DirectionalLight(0xffeedd)
+    var directionalLight = new THREE.DirectionalLight(0xffeedd);
     // 指定定向光源由z正半轴射向原点（平行光从屏幕外射向屏幕中心）
     directionalLight.position.set(0, 0, 1);
 
@@ -74,6 +80,9 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
                             child.material.lights = false;
                         }
                     }
+
+                    _handle_child_material(child);
+
                     // 初始化动画参数
                     _dealObjectInLoadCirculStart(child);
                     if (child.material) {
@@ -130,7 +139,7 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
                                     _afterMovementMesh(root, scene, camera, renderer);
 
                                     // 绑定事件，比如鼠标移到板块上高亮
-                                    _initListener();
+                                    _initListener(_config, object);
                                 }
                             }.bind(this));
                         tween.start();
@@ -243,7 +252,7 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
 					_setHeightSlow(child, height);
 					child.visible = true;
 					// var o = object.getObjectByName(dmName);
-					// o._data = data;
+					// o._data = child.userData;
 				}
 			} else {
 				dmName = name ? name.split("_")[0] : '';
@@ -289,16 +298,16 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
     /**
 	 * 设置模型高度
 	 * 这个方法不要加太多逻辑，这里我试着在循环里console.log，渣电脑甚至能掉帧
-	 * @param object
+	 * @param child
 	 * @param height
 	 * @private
 	 */
-	function _setHeight(object, height) {
+	function _setHeight(child, height) {
 		if (height === 0 || isNaN(height)) {
 			// 不能高度设置为0，否则下一次设置的时候会出问题，
 			height = 1;
 		}
-		var geometry = object.geometry;
+		var geometry = child.geometry;
 
 		geometry.verticesNeedUpdate = true;
 		var vertices = geometry.vertices;
@@ -331,11 +340,19 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
     /**
 	 * 事件绑定
 	 * @private
+     * @param config 模型配置文件
+     * @param object .obj文件，所有模型数据。这里得注意跟child的区别，变量名写惯了都是object..
 	 */
-	function _initListener() {
+	function _initListener(config, object) {
 		var dom = document.getElementById('container');
         
-        // dom.addEventListener("mousemove", _setMeshHighLightStatus, false);
+        dom.addEventListener('mousemove', function(e) {
+            return _setMeshHighLightStatus(e, config, object);
+        }, false);
+
+        dom.addEventListener('click', function(e) {
+            return _showDetail(e, config, object);
+        }, false);
     };
     
     /* 
@@ -345,48 +362,98 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
         移开后恢复原来的材质
         得注意边界线也是种模型，需要额外判断
     */
-    function _setMeshHighLightStatus(event) {
-        event.preventDefault();
-
-        var intersected = _objectFromMouse(event.pageX, event.pageY);
+    function _setMeshHighLightStatus(event, config, object) {
+        var intersected = _objectFromMouse(event.pageX, event.pageY, [object]);
         var object = intersected.object;
-
-        // 工具类
-        var model = {
-            current: {}, // 当前选中板块对象
-            old: {}, // 第二次及以后选中的板块对象
-            getCurrent: function() {
-                return this.current;
-            },
-            getOld: function() {
-                return this.old;
-            },
-            getUuid: function() {
-                return this.current.uuid ? this.current.uuid : null;
-            },
-            set: function(current, old) {
-                this.current = current;
-                this.old = old;
-            }
-        };
 
         // 设置/移除高亮
         if(object) {
-            console.log(object)
-            var uuid = model.getUuid();
+            var uuid = this._current && this._current.uuid;
             if(uuid === object.uuid) {
                 return;
             }else {
                 if(!uuid){ // 第一次
-
-                    object.material.color.set('#F96');
+                    this._current = object;
+                    object.material.color.set(config.select_color);
                 }else {
+                    this._current.material.color.set(config.top_color);
+
+                    this._old = this._current;
+                    this._current = object;
                     
+                    object.material.color.set(config.select_color);
                 }
+
             }
         }else { // 重置所有板块材质
             
         }
+    }
+
+    /* 
+        点击板块，板块左移，右边空出来的地方显示表格
+    */
+    function _showDetail(event, config, object) {
+        event.preventDefault();
+
+        var intersected = _objectFromMouse(event.pageX, event.pageY, [object]);
+        var child = intersected.object;
+
+        if(child){
+            _meshMove(true, object);
+            _showTable(child);
+        }
+    }
+
+    /**
+	 * 整个模型漂移
+	 * 如果withdraw为true，表示需要把模型移开，即从中心点向左移
+	 *
+	 * 如果withdraw为false，表示需要还原至中心
+	 * @param withdraw 是否移开
+	 * @private
+	 */
+	function _meshMove(withdraw, object) {
+		var point = { x: 0, y: 0, z: 0 };
+		var rotation = { x: -Math.PI / 4, y: 0, z: 0 };
+		var root = this.root.rotation;
+
+		if (withdraw) {
+			rotation = { x: -Math.PI / 4, y: Math.PI / 180 * 10, z: 0 };
+            
+            // 默认配比是500*300的设置
+            var gap = document.getElementById('container').clientWidth / 550;
+            // 算移开的位置，移开的位置是固定的，只算一遍
+			if (!this._withdrawPosition) {
+				this._withdrawPosition = _getCoordinate2InScene({
+					x: 30 * gap,
+					y: this.renderer.domElement.offsetHeight / 2
+				}, this.camera, this.renderer.domElement);
+
+                // x 的位置需要加上模型宽的一半，避免飞出
+				this._withdrawPosition.x += getMeshWidth(object) / 2;
+			}
+			point = this._withdrawPosition;
+		}
+
+		if (this._meshTween) {
+			TWEEN.remove(this._meshTween);
+			this._meshTween = null;
+        }
+		this._meshTween = _tweenInOut(object.position, { x: point.x, y: 0, z: 0 });
+    };
+    
+    /* 获得模型宽度 */
+    function getMeshWidth(object) {
+        var c = 16711680, length = 0;
+        var boxHelper = new THREE.BoxHelper(object, c);
+        var box = new THREE.Box3().setFromObject(object);
+
+        boxHelper.update();
+
+        length = box.max.x - box.min.x;
+
+        return length;
     }
 
     // 获得元素相对整个页面的偏移量
@@ -408,7 +475,7 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
     }
 
     // 获得鼠标位置的板块模型对象
-    function _objectFromMouse(pagex, pagey) {
+    function _objectFromMouse(pagex, pagey, object) {
         var offset = getOffset(this.renderer.domElement);
         var eltx = pagex - offset.left;
         var elty = pagey - offset.top;
@@ -418,7 +485,8 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
         var vector = new THREE.Vector2(vpx, vpy);
         var raycaster = new THREE.Raycaster();
         raycaster.setFromCamera(vector, this.camera);
-        var intersects = raycaster.intersectObjects(this.root.children, true);
+        // var intersects = raycaster.intersectObjects(this.root.children, true);
+        var intersects = raycaster.intersectObjects(object, true);
         var len = intersects.length;
         if (len > 0) {
             var intersect = intersects[0];
@@ -432,4 +500,123 @@ require(['three', 'mtl-loader', 'obj-loader', 'orbitControls', 'tween'], functio
             face: null
         };
     }
+
+    function _handle_child_material(child) {
+        if (child instanceof THREE.Mesh || child instanceof THREE.Line) {
+            var name = child.name.split('_');
+            var last_name = name[name.length - 1];
+
+            // 改变模型贴图
+            switch(last_name){
+                case 'Line': // 内部乡镇边界贴图
+                    child.material.color.set(_config.line_color);
+                break;
+
+                case 'pillar': // 柱子贴图
+                    child.material.color.set(_config.pillar_color);
+                    // child.material.map = new THREE.TextureLoader().load("../assets/texture/crate.jpg");
+                break;
+
+                case 'bottom': // 底部贴图
+                    child.material.color.set(_config.bottom_color);
+                break;
+            }
+
+            if(name.length == 1){ // 上层表面贴图
+                child.material.color.set(_config.top_color);
+                // child.material.opacity = 0.1;
+            }
+        }
+    }
+
+    function _getCoordinate2InScene(i, d, c) {
+        d.updateMatrixWorld(true);
+        var b = getVector2InScene(i, c);
+        var j = new THREE.Vector3(b.x, b.y, 0);
+        j.unproject(d);
+        j.sub(d.position);
+        j.normalize();
+        var f = new THREE.Raycaster(d.position, j);
+        var h = f.ray.origin;
+        var g = f.ray.direction;
+        var e = 0;
+        var a = new THREE.Vector3();
+        a.setX(h.x - ((h.z - e) * g.x / g.z));
+        a.setY(h.y - ((h.z - e) * g.y / g.z));
+        return a;
+    }
+
+    function getVector2InScene(a, c) {
+        var b = new THREE.Vector2();
+        b.x = (a.x / c.offsetWidth) * 2 - 1;
+        b.y = -(a.y / c.offsetHeight) * 2 + 1;
+        return b;
+    };
+
+    /**
+	 * TWEEN动画，封装一下调用的时候写简单点
+	 * @param a
+	 * @param b
+	 * @private
+	 */
+	function _tweenInOut(a, b, c, s) {
+		var tween = new TWEEN.Tween(a)
+			.to(b, this._duration)
+			.easing(TWEEN.Easing.Exponential.InOut);
+		if(c) {
+			tween.onComplete(c);
+		}
+		if(s) {
+			tween.onStart(s);
+		}
+		return tween.start();
+    };
+    
+    /**
+	 * 右侧表格数据的显示
+	 * @param child
+	 * @private
+	 */
+	function _showTable(child) {
+        var detail = document.getElementById('detail');
+
+		// if (detail.children.length != 0) {
+
+		// } else {
+			// 如果表格不存在，创建
+			_createTable(child, detail);
+		// }
+    };
+    
+    /**
+	 * 创建表格元素
+	 * @private
+	 */
+	function _createTable(child, element) {
+        var data = child.userData;
+        var table = '';
+        console.log(data)
+
+        table += 
+        '<table border="0" cellspacing="0" cellpadding="0" class="detail-table">' +
+            '<tr class="header"> ' + 
+                '<td></td>' + 
+                '<td>' + data.stnm + '</td>' +
+            '</tr>'+
+
+            '<tr class="decorate">' + 
+                '<td colspan="5"></td>' + 
+            '</tr>' + 
+
+            '<tr>' + 
+                '<td class="title">' + '水位' + '</td>' +
+                '<td class="value">' + data.val + '</td>' +
+            '</tr>' +
+
+            '<tr class="decorate">' + 
+                '<td colspan="5"></td>' + 
+            '</tr>' +
+        '</table>';
+        element.innerHTML += table;
+	};
 });
